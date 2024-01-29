c018e3a77c094cde8123e9813621c4465d5973e6
bouhvli <bouhali.hamza.2@gmail.com>
2024-01-29 14:02:54 +0100

    adding new line

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,6 +1,6 @@
 // importing jquery
 import $ from "jquery";
 
-$('body').append(`<p>Holberton Dashboard</p>`);
-$('body').append(`<p>Dashboard data for the students</p>`);
-$('body').append(`<p>Copyright - Holberton School</p>`);
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<p>Copyright - Holberton School</p>');
