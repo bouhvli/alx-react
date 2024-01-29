e5408a25fff5bd6d5b39f8ef1084852d958405b3
bouhvli <bouhali.hamza.2@gmail.com>
2024-01-29 18:23:10 +0100

    update

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,6 +1,6 @@
 // importing jquery and lodash
-import $ from 'jquery';
-import _ from 'lodash';
+const $ = require('jquery');
+const _ = require('lodash');
 
 
 $('body').append('<p>Holberton Dashboard</p>');
@@ -14,6 +14,6 @@ const updateCounter = _.debounce(() => {
   let count = parseInt($('#count').text().replace(' clicks on the button', '')) || 0;
   count++;
   $('#count').text(`${count} clicks on the button`);
-}, 500);
+}, 500, {loading: true, trailing: false});
 
 $('button').on('click', updateCounter);
