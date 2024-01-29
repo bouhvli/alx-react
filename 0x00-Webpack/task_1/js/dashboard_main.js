87a1814a4ae8844c39752a54dca27f744abb971a
bouhvli <bouhali.hamza.2@gmail.com>
2024-01-29 16:27:03 +0100

    update

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -3,13 +3,12 @@ import $ from 'jquery';
 import _ from 'lodash';
 
 
-$('body').append(
-  `<p>Holberton Dashboard</p>
-  <p>Dashboard data for the students</p>
-  <button>Click here to get started</button>
-  <p id='count'></p>
-  Copyright - Holberton School`
-);
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append("<p id='count'></p>");
+$('body').append('Copyright - Holberton School');
+
 
 const updateCounter = _.debounce(() => {
   let count = parseInt($('#count').text().replace(' clicks on the button', '')) || 0;
