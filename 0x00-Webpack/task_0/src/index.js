6035b5c89f1e1f55c98ae40fc22a313a93e81600
bouhvli <bouhali.hamza.2@gmail.com>
2024-01-29 13:39:05 +0100

    first task

---
+++ 0x00-Webpack/task_0/src/index.js
@@ -0,0 +1,9 @@
+import $ from "jquery";
+
+const paragraphOne = `Holberton Dashboard`;
+const paragraphTwo = `Dashboard data for the students`;
+const paragraphthree = `Copyright - Holberton School`;
+
+$('body').append(`<p>${paragraphOne}</p>`);
+$('body').append(`<p>${paragraphTwo}</p>`);
+$('body').append(`<p>${paragraphthree}</p>`);
