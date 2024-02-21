875df903bf622b1fb87c578857c2e13f2a059428
bouhvli <bouhali.hamza.2@gmail.com>
2024-02-21 17:22:19 +0100

    update

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,12 +1,13 @@
-const { fromJS, Map } = require('immutable');
+import { Map } from 'immutable';
+
 export default function accessImmutableObject(object, array) {
-    let current = object;
-    for (const key of array) {
-        current = current[key];
-    }
-    if (typeof current === 'string' || current instanceof Map) {
-        return current
-    } else {
-        return undefined
-    }
+  let current = object;
+  for (const key of array) {
+    current = current[key];
+  }
+  if (typeof current === 'string' || current instanceof Map) {
+    return current
+  } else {
+    return undefined
+  }
 }
