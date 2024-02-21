6440e32f7209c0dd8db8af1ca2a3c193ae784df8
bouhvli <bouhali.hamza.2@gmail.com>
2024-02-21 18:26:14 +0100

    update

--- 0x06-React_Immutable/7-equality.js
+++ 0x06-React_Immutable/7-equality.js
@@ -1,17 +1,5 @@
 import { is } from 'immutable';
 
 export default function areMapsEqual(map1, map2) {
-  if (map1 === map2) {
-    return true;
-  }
-  if (map1.size !== map2.size) {
-    return false;
-  }
-  for (const [key, value] of map1) {
-    const value_m2 = map2.get(key);
-    if (!is(value, value_m2)) {
-      return false;
-    }
-  }
-  return true;
+  return is(map1, map2);
 }
