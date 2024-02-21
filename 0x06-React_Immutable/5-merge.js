c01a431982ec71f9c486a0072362e55f22068ac7
bouhvli <bouhali.hamza.2@gmail.com>
2024-02-21 18:20:24 +0100

    update

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -1,15 +1,9 @@
 import { Map, List } from 'immutable';
 
 export function concatElements(page1, page2) {
-  const li1 = List(page1);
-  const li2 = List(page2);
-
-  return (li1.concat(li2));
+  return (List(page1).concat(List(page2)));
 }
 
 export function mergeElements(page1, page2) {
-  const m1 = Map(page1);
-  const m2 = Map(page2);
-
-  return (m1.merge(page2));
+  return (Map(page1).merge(List(page2)));
 }
