c814751a7c0281c12727d4676ea38e7fc6d67395
bouhvli <bouhali.hamza.2@gmail.com>
2024-02-21 18:22:57 +0100

    update

--- 0x06-React_Immutable/6-deeply.js
+++ 0x06-React_Immutable/6-deeply.js
@@ -1,8 +1,5 @@
 import { fromJS, Map } from 'immutable';
 
 export default function mergeDeeplyElements(page1, page2) {
-  const map1 = Map(page1);
-  const map2 = Map(page2);
-
   return fromJS(page1).mergeDeep(page2);
 }
