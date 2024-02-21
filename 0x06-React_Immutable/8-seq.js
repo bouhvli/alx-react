af9edf6a66cf2f295afa88488d82dd4f0a99c11f
bouhvli <bouhali.hamza.2@gmail.com>
2024-02-21 18:44:03 +0100

    update

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -1,16 +1,16 @@
-const { Seq } = require('immutable');
+import { Seq } from 'immutable';
 
 export default function printBestStudents(grades) {
-    const fltr = Seq(grades)
-        .filter(std => std.score >= 70)
-        .map(std => ({
-            ...std,
-            firstName: cap(std.firstName),
-            lastName: cap(std.lastName),
-        })).toObject();
-    console.log(fltr);
+  const fltr = Seq(grades)
+    .filter(std => std.score >= 70)
+    .map(std => ({
+      ...std,
+      firstName: cap(std.firstName),
+      lastName: cap(std.lastName),
+    })).toObject();
+  console.log(fltr);
 }
 
 function cap(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
